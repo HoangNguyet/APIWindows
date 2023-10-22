d99cf1f29000e205cf59275de72f52a4809019cf
HoangNguyet <nguyethoanghan@gmail.com>
2023-10-23 00:03:15 +0700

    Openfile

---
+++ APIWin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd);
+
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        // Tạo một form
+        Form form = new Form();
+        form.Text = "Ứng dụng Mở Tệp";
+        form.Size = new System.Drawing.Size(400, 200);
+
+        // Tạo textbox để nhập đường dẫn tệp
+        TextBox textBox = new TextBox();
+        textBox.Location = new System.Drawing.Point(10, 10);
+        textBox.Size = new System.Drawing.Size(300, 24);
+
+        // Tạo button để mở tệp
+        Button openFileButton = new Button();
+        openFileButton.Text = "Mở tệp";
+        openFileButton.Location = new System.Drawing.Point(10, 40);
+        openFileButton.Click += (sender, e) => OpenFile(textBox.Text);
+        form.Controls.Add(textBox);
+        form.Controls.Add(openFileButton);
+
+        // Hiển thị form
+        Application.Run(form);
+    }
+
+    static void OpenFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            ShellExecute(IntPtr.Zero, "open", filePath, null, null, 1);
+        }
+        else
+        {
+            MessageBox.Show("Tệp không tồn tại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
