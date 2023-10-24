0a5741ec69fb69e80a804dba4770ea86860e0266
hihiha <khacninh2020@gmail.com>
2023-10-24 16:52:07 +0700

    Fix

--- APIWin/module/SearchFile.cs
+++ APIWin/module/SearchFile.cs
@@ -14,18 +14,14 @@ namespace APIWin.module
         public static List<WIN32_FIND_DATA> getFile(string path)
         {
             List<WIN32_FIND_DATA> fileContaner = new List<WIN32_FIND_DATA>();
-            List<WIN32_FIND_DATA> temp = new List<WIN32_FIND_DATA>();
             WIN32_FIND_DATA findData;
-            WIN32_FIND_DATA findData1;
 
             FINDEX_INFO_LEVELS findInfoLevel = FINDEX_INFO_LEVELS.FindExInfoBasic;
-            FINDEX_INFO_LEVELS findInfoLevel1 = FINDEX_INFO_LEVELS.FindExInfoBasic;
 
             int additionalFlags = 0;
 
 
             string pattern = $"{path}\\*.*";
-            string pattern1 = $"{path}\\*.";
 
 
             IntPtr hFile = FindFirstFileEx(
@@ -39,38 +35,14 @@ namespace APIWin.module
             {
                 do
                 {
-                    fileContaner.Add(findData);
+                    if ((findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+                    {
+                        fileContaner.Add(findData);
+                    }
 
                 }
                 while (FindNextFile(hFile, out findData));
 
-            }
-            IntPtr hFile1 = FindFirstFileEx(
-            pattern1,
-            findInfoLevel1,
-            out findData1,
-            FINDEX_SEARCH_OPS.FindExSearchNameMatch,
-            IntPtr.Zero,
-            additionalFlags);
-            if (hFile1.ToInt32() != -1)
-            {
-                do
-                {
-                    temp.Add(findData1);
-
-                }
-                while (FindNextFile(hFile1, out findData1));
-
-            }
-            for (int i = 0; i < fileContaner.Count; i++)
-            {
-                foreach (var item in temp)
-                {
-                    if (fileContaner[i].cFileName == item.cFileName)
-                    {
-                        fileContaner.RemoveAt(i);
-                    }
-                }
             }
             return fileContaner;
         }
